709f331266c077dbe3dfe0f4c25c4a580e24c606
Julia Nekoz <yulyanekoz@yandex.ru>
2026-06-01 23:29:13 +0300

    Обновил fit_life.py

--- fit_life.py
+++ fit_life.py
@@ -1,6 +1,7 @@
 # Проект FitLife - MVP версия 1.0
 import sys
 import io
+
 sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
