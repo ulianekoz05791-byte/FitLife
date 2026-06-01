8661faf0b5b2045951f8f96b991258907f096e98
Julia Nekoz <yulyanekoz@yandex.ru>
2026-06-01 23:40:21 +0300

    Обновил fit_life.py

--- fit_life.py
+++ fit_life.py
@@ -6,26 +6,24 @@ import io
 sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8')
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-WATER_PEG_KG = 30
+WATER_PER_KG_ML = 30  
 ML_IN_LITRE = 1000
 
-user_name = input('Доброго времени суток, пожайлуста, напишите ваше имя:')
+user_name = input('Доброго времени суток, пожалуйста, напишите ваше имя: ')
 
-user_age = int(input('Введите Ваш возраст:'))
+user_age = int(input('Введите ваш возраст: '))
 
-user_weight = float(input('Введите Ваш вес в кг. (пример: 56.6):'))
+user_weight = float(input('Введите ваш вес в кг (пример: 56.6): '))
 
-user_height = float(input('Введите ваш рост в метрах (пример: 1.77):'))
+user_height = float(input('Введите ваш рост в метрах (пример: 1.77): '))
 
-bmi = round(user_weight / (user_height**2), 1)
+bmi = round(user_weight / (user_height ** 2), 1)
 
-water_ml = user_weight * WATER_PEG_KG
-water_l = water_ml / 1000
+water_ml = user_weight * WATER_PER_KG_ML
+water_l = water_ml / ML_IN_LITRE
 
 print()
-
-print(f'Отчёт для пользователя: {user_name} ({user_age} г.)')
+print(f'Отчёт для пользователя: {user_name} ({user_age} лет)')
 print(f'Твой Индекс Массы Тела: {bmi}')
-print(f'Рекомендуемая норма воды: {water_l:.1f} л. в день ')
-
-print("Расчет окончен. Будьте здоровы!")
+print(f'Рекомендуемая норма воды: {water_l:.1f} л. в день')
+print('Расчёт окончен. Будьте здоровы!')
